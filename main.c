dd62887bea45b6e8d549941c4c0be5dc83f82b2e
FlorianCogoni <floco10@hotmail.fr>
2017-02-12 19:00:46 +0100

    ajout main

--- main.c
+++ main.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "structure.h"
-#include "fonction.h"
-
-
-
-
+#include "fonctions.h"
+#include "structures.h"
 
 int main()
 {
     agenda_t ** pag = creation("test.txt");
-    printf("%s %s %s \n", (*pag)->date, (*pag)->actions->moment, (*pag)->actions->nom);
+    printf("%s %s %s \n", (*pag)->date, (*(*pag)->actions)->moment, (*(*pag)->actions)->nom);
     return 0;
 }
