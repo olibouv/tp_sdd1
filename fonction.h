f96dfbfe5e17d29574eb0fe64a920da2d7568b19
olibouv <oli.bouv@yahoo.fr>
2017-03-09 23:23:37 +0100

    Update fonction.h

--- fonction.h
+++ fonction.h
@@ -1,6 +1,16 @@
 #ifndef FONCTION
 #define FONCTION
 
+#include "structures.h"
+
 agenda_t ** creation(char nomfichier[]);
+
+void sauvegarde (char nom[], agenda_t ** pag);
+
+int supprime (char date[6],char moment[3], agenda_t ** pag);
+
+int trouvemotif(char motif[], char nom[10]);
+
+char *** liste_action(char motif[], agenda_t ** pag);
 
 #endif
