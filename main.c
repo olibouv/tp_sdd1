979e25ef39398dd51d6714e0d1628611ed3d9614
FlorianCogoni <floco10@hotmail.fr>
2017-03-10 16:11:17 +0100

    Update main.c
    test main

--- main.c
+++ main.c
@@ -3,47 +3,71 @@
 #include <string.h>
 #include "fonctions.h"
 #include "structures.h"
+#deifne TMAX 100
 
 
 
-/* Problème d'insertion !!!!!!!!!!!!!!!!!!!!!*/
-
-int main()
-{
-    /*agenda_t ** pag = creation("test.txt");*/
-    agenda_t *ag = (agenda_t *) malloc(sizeof(agenda_t));
-    action_t *ac = (action_t *) malloc(sizeof(action_t));
-    strcpy(ag->date, "123456");
-    strcpy(ac->nom, "yo ça va ?");
-    strcpy(ac->moment, "42");
-    ac->suivant = NULL;
-    ag->actions = &ac;
-    agenda_t *ag1 = (agenda_t *) malloc(sizeof(agenda_t));
-    action_t *ac1 = (action_t *) malloc(sizeof(action_t));
-    strcpy(ag1->date, "654321");
-    strcpy(ac1->nom, "coucoucou!");
-    strcpy(ac1->moment, "24");
-    ag1->suivant = NULL;
-    ag1->actions = &ac1;
-    action_t *ac2 = (action_t *) malloc(sizeof(action_t));
-    strcpy(ac2->nom, "yoyoyoyoyo");
-    strcpy(ac2->moment, "88");
-    ac1->suivant = ac2;
-    ag->suivant = ag1;
-    agenda_t **pag2 = &ag;
-    /*int cd = supprime("654321", "24", pag2);
-    if (cd)
-    {
-        printf("suppresssion réussie \n");
-    }
-    else
-    {
-        printf("suppression ratée \n");
-    }
-    sauvegarde("test2.txt", pag2);*/
-    /*printf("%s %s %s \n",(*pag)->date,(*(*pag)->actions)->moment,(*(*pag)->actions)->nom);
-    printf("%s %s %s \n", (*pag)->suivant->date, (*(*pag)->suivant->actions)->moment, (*(*pag)->suivant->actions)->nom);*/
-    char *** tete = liste_action("yo",pag2);
-    printf(" %s \n %s \n", *tete[0], *(tete[0])+sizeof(char[10]));
-    return 0;
-}
+int main()                                                                                      
+{                                                                                               
+    printf("test creation : résultat testcreation.txt \n");                                     
+    agenda_t ** pag = creation("test.txt");                                                     
+    sauvegarde("testcreation.txt",pag);                                                         
+    /*creation manuelle d'un agenda*/                                                           
+    agenda_t *ag = (agenda_t *) malloc(sizeof(agenda_t));                                       
+    action_t *ac = (action_t *) malloc(sizeof(action_t));                                       
+    strcpy(ag->date, "200405");                                                                 
+    strcpy(ac->nom, "TPs de SDD");                                                              
+    strcpy(ac->moment, "112");                                                                  
+    ac->suivant = NULL;                                                                         
+    ag->actions = &ac;                                                                          
+    agenda_t *ag1 = (agenda_t *) malloc(sizeof(agenda_t));                                      
+    action_t *ac1 = (action_t *) malloc(sizeof(action_t));                                      
+    strcpy(ag1->date, "201842");                                                                
+    strcpy(ac1->nom, "TPs de CSN");                                                             
+    strcpy(ac1->moment, "420");                                                                 
+    ag1->suivant = NULL;                                                                        
+    ag1->actions = &ac1;                                                                        
+    action_t *ac2 = (action_t *) malloc(sizeof(action_t));                                      
+    strcpy(ac2->nom, "Cours de C#");                                                            
+    strcpy(ac2->moment, "518");                                                                 
+    ac1->suivant = ac2;                                                                         
+    ag->suivant = ag1;                                                                          
+    agenda_t **pag2 = &ag;                                                                      
+    /*printf("test creation liste contigue motif présent : \n");                                
+    char *** tete1 = liste_action("TPs",pag2);                                                  
+    char * cour = (char *) malloc(sizeof(char[10]));                                            
+    for (cour = *tete1[1]; cour < *tete1[2]; cour+=sizeof(char[10]))                            
+    {                                                                                           
+        printf(" %s \n ", cour);                                                                
+    }                                                                                           
+    printf("test creation liste contigue motif inexistant : \n");                               
+    for (cour = *tete1[1]; cour < *tete1[2]; cour+=sizeof(char[10]))                            
+    {                                                                                           
+      printf(" %s \n ", cour);                                                                  
+    }  */                                                                                       
+    printf("test suppression élément inexistant :\n");                                          
+    int cd1 = supprime("000000","00",pag2);                                                     
+    if (cd1)                                                                                    
+    {                                                                                           
+        printf("suppression réussie\n");                                                        
+    }                                                                                           
+    else                                                                                        
+    {                                                                                           
+        printf("suppression ratée\n");                                                          
+    }                                                                                           
+    sauvegarde("testsup1.txt", pag2);                                                           
+    printf("sauvegarde testsup1.txt \n");                                                       
+    printf("test suppression élément existant :\n");                                            
+    int cd2 = supprime("201842", "420", pag2);                                                  
+    if (cd2)                                                                                    
+    {                                                                                           
+        printf("suppression réussie \n");                                                       
+    }                                                                                           
+    else                                                                                        
+    {                                                                                           
+        printf("suppression ratée \n");                                                         
+    }                                                                                           
+    sauvegarde("testsup2.txt", pag2);                                                           
+    printf("sauvegarde testsup2.txt \n");                                                       
+    return 0;                                                                                   
+}                                                                                               
