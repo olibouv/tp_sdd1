aaa7967fe3740ecdaa4eaa00017d767cce649537
olibouv <oli.bouv@yahoo.fr>
2017-03-10 02:05:46 +0100

    Update fonctions.h

--- tp_sdd1-cogo/fonctions.h
+++ tp_sdd1-cogo/fonctions.h
@@ -23,7 +23,7 @@ void sauvegarde (char nom[], agenda_t ** pag);
 
 int supprime (char date[6],char moment[3], agenda_t ** pag);
 
-int trouve_motif(char motif[], char nom[10]);
+int trouvemotif(char motif[], char nom[10]);
 char *** liste_action(char motif[], agenda_t ** pag);
 
 #endif
