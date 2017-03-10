7dd61adebde8304286f770efa04b1d11461efca9
olibouv <oli.bouv@yahoo.fr>
2017-03-10 02:05:11 +0100

    Update fonctions.h

--- tp_sdd1-cogo/fonctions.h
+++ tp_sdd1-cogo/fonctions.h
@@ -1,3 +1,17 @@
+/*
+ ______________________________________________________________________________________
+|                                                                                      |
+|                              ___________________                                     |
+|                             |                   |                                    |
+|                             |    fonctions.h    |                                    |
+|                             |___________________|                                    |
+|                                                                                      |
+|______________________________________________________________________________________|
+	Ce fichier contient les prototypes fonctions utilisées dans ce TP.
+*/
+
+
+
 #ifndef FONCTION
 #define FONCTION
 
@@ -9,8 +23,7 @@ void sauvegarde (char nom[], agenda_t ** pag);
 
 int supprime (char date[6],char moment[3], agenda_t ** pag);
 
-int trouvemotif(char motif[], char nom[10]);
-
+int trouve_motif(char motif[], char nom[10]);
 char *** liste_action(char motif[], agenda_t ** pag);
 
 #endif
